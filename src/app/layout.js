48d9a868ce3f264c6534095e44317f12285ed5a2
readul-islam <developer.readul@gmail.com>
2024-02-24 18:18:05 +0600

    update

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,7 +2,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
-
+// hello
 export const metadata = {
   title: "onveta",
   description: "Explore Onveta, the ultimate Tailwind design component library, featuring a comprehensive collection of forms, dividers, heroes, login pages, navbars, footers, steps, layouts, and more. Streamline your UI development process with our meticulously crafted components. Start building beautiful, responsive web interfaces effortlessly. ailwind CSS components, UI design library, Tailwind UI kit, responsive web design, UI components, Tailwind CSS forms, Tailwind dividers, Tailwind heroes, Tailwind login pages, Tailwind navbars, Tailwind footers, Tailwind steps, web development resources",
